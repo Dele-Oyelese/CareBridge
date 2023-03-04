dff80168f73f06db96fbb0a2d9f23bb4f0a0ea4c
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-03-04 11:09:29 -0700

    update part 1/ adjust scss/ adjust page

---
+++ frontend/src/pages/update.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./update.scss";
+
+export const Update = () => {
+  return (
+    <div class='container'>
+      <h1 class='text'> You have now a new update: </h1>
+      <h1 class='text update1'> hello </h1>
+      <h1> </h1>
+    </div>
+  );
+};
