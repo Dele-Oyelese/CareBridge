e6aa0e487cc1442c5a1602b2e4f6499cddaa0ef4
Dele <doyelese@gmail.com>
2023-03-04 14:46:34 -0700

    got rid of lupus es

--- frontend/src/components/js/ExecutiveSummary.js
+++ frontend/src/components/js/ExecutiveSummary.js
@@ -10,13 +10,21 @@ function Summary() {
         <div className="summary-container">
         <h1 > About Care Bridge </h1>
         <p>
-        Lorem ipsum dolor sit amet, 
-        consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut 
-        labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, 
-        quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
+        CareBridge is an innovative communication tool to link you as an important part of a patient’s care journey. CareBridge will send you automatic updates in real time from a state of the art system. You can get updates such as: 
+
+When your loved one is going for tests
+
+When a member of the physician or allied health team is seeing your loved one 
+
+Update from a surgical team or procedure team on how your loved one is doing and how the procedure is progressing including photos and video updates from the team. 
+
+Who is taking care of your loved one today 
+
+When they are moving to another area of the hospital 
+
+Update on their status, if they are stable, comfortable and resting or up walking 
+
+Let CareBridge connect you with your loved one in hospital, even when you cannot be there. 
         </p> 
         </div>
 
