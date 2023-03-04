1f3f3499076a3ac808bf8f30bb6cc0a9c0ee35a5
Louis-Antoine Etchian <thelaylife@gmail.com>
2023-03-04 11:31:29 -0700

    Merged notification pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import {Notification} from "./pages/Notification";
+import {Update} from "./pages/update";
 
 function App() {
   return (
