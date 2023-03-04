46b37ddbcd42cf06a92b3313a91bde38bb4d2f81
Louis-Antoine Etchian <thelaylife@gmail.com>
2023-03-04 14:58:57 -0700

    Merged frontend

--- frontend/src/pages/Notification/Notification.js
+++ frontend/src/pages/Notification/Notification.js
@@ -1,97 +1,95 @@
-import React, { useState, useEffect } from "react";
-import Modal from "react-modal";
+import React, {useState, useEffect} from 'react';
+import Modal from 'react-modal';
 import "./Notification.scss";
 
-const availableTime = 5;
+const availableTime = 30
 
 const notifications = [
-  {
-    id: 0,
-    title: "Patient general update (2023-03-04 10:00)",
-    message: "Patient X received a bath.",
-  },
-  {
-    id: 1,
-    title: "Patient general update (2023-03-04 11:00)",
-    message: "Patient X were examined by Doctor H.",
-  },
-  {
-    id: 2,
-    title: "Patient transfer (2023-03-04 15:00)",
-    message:
-      "Patient X were transferred to the ER following breathing problems.",
-  },
+    {id: 453645645645, title: 'New Patient Update (03/04/2023, 10:20:15)', message: 'Patient X received a bath.'},
+    {
+        id: 453645645646,
+        title: 'New Patient Update (03/04/2023, 11:54:03)',
+        message: 'Patient X were examined by Doctor H.'
+    },
+    {
+        id: 453645645647, title: 'New Patient Update (03/04/2023, 14:10:00)',
+        message: 'Patient X were transferred to the ER following breathing problems.'
+    }
 ];
 
 export const Notification = () => {
-  const [selectedNotification, setSelectedNotification] = useState(null);
-  const [remainingNotifications, setRemainingNotifications] =
-    useState(notifications);
-  const [timeLeft, setTimeLeft] = useState(null);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (timeLeft) {
-        setTimeLeft(timeLeft - 1);
-      } else if (timeLeft === 0) {
-        closeModal(selectedNotification);
-      }
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [timeLeft]);
+    const [selectedNotification, setSelectedNotification] = useState(null);
+    const [remainingNotifications, setRemainingNotifications] = useState(notifications);
+    const [timeLeft, setTimeLeft] = useState(null);
 
-  const openModal = (notification) => {
-    setSelectedNotification(notification);
-    setTimeLeft(availableTime);
-  };
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (timeLeft) {
+                setTimeLeft(timeLeft - 1);
+            } else if (timeLeft === 0) {
+                closeModal(selectedNotification);
+            }
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [timeLeft]);
 
-  const closeModal = (notification) => {
-    setRemainingNotifications((prevNotifications) =>
-      prevNotifications.filter((n) => n.id !== notification.id)
-    );
-    setSelectedNotification(null);
-    setTimeLeft(null);
-  };
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            const response = await fetch('http://localhost:5000/get_patient_update');
+            const newNotifications = await response.json();
+            for (let i = 0; i < newNotifications.length; i++) {
+                setRemainingNotifications((prevNotifications) => [...prevNotifications, newNotifications[i]]);
+            }
+            fetchNotifications();
+        };
 
-  return (
-    <div className='notification'>
-      <div className='notification__header'>You have now a new update:</div>
-      <div className='notification__list'>
-        {remainingNotifications.map((notification) => (
-          <div
-            key={notification.id}
-            className='notification__list-item'
-            onClick={() => openModal(notification)}
-          >
-            <h2>{notification.title}</h2>
-          </div>
-        ))}
-      </div>
-      <Modal
-        isOpen={!!selectedNotification}
-        onRequestClose={() => closeModal(selectedNotification)}
-        contentLabel='Notification Details'
-        className='notification__modal'
-        overlayClassName='notification__overlay'
-      >
-        {selectedNotification && (
-          <>
-            <h2 className='notification__modal__header'>
-              {selectedNotification.title}
-            </h2>
-            <p className='notification__modal__description'>
-              {selectedNotification.message}
-            </p>
-            <p className='notification__modal__time'>Time left: {timeLeft}s</p>
-            <button
-              className='notification__modal__button'
-              onClick={() => closeModal(selectedNotification)}
+        fetchNotifications();
+
+    }, []);
+
+    const openModal = (notification) => {
+        setSelectedNotification(notification);
+        setTimeLeft(availableTime);
+    };
+
+    const closeModal = (notification) => {
+        setRemainingNotifications((prevNotifications) =>
+            prevNotifications.filter((n) => n.id !== notification.id)
+        );
+        setSelectedNotification(null);
+        setTimeLeft(null);
+    };
+
+    return (
+        <div className='notification'>
+            <div className='notification__header'>You have now a new update:</div>
+            <div className="notification__list">
+                {remainingNotifications.map((notification) => (
+                    <div key={notification.id} className="notification__list-item" onClick={() => openModal(notification)}>
+                        <h2>{notification.title}</h2>
+                    </div>
+                ))}
+            </div>
+            <Modal
+                isOpen={!!selectedNotification}
+                onRequestClose={() => closeModal(selectedNotification)}
+                contentLabel="Notification Details"
+                className="notification__modal"
+                overlayClassName="notification__overlay"
             >
-              OK
-            </button>
-          </>
-        )}
-      </Modal>
-    </div>
-  );
-};
+                {selectedNotification && (
+                    <div>
+                        <h2 className='notification__modal__header'>{selectedNotification.title}</h2>
+                        <p className='notification__modal__description'>{selectedNotification.message}</p>
+                        <p className='notification__modal__time'>Time left: {timeLeft}s</p>
+                        <button className="notification__modal__button"
+                                onClick={() => closeModal(selectedNotification)}>
+                            OK
+                        </button>
+                    </div>
+                )}
+            </Modal>
+        </div>
+    );
+}
