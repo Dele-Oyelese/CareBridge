74e3f74380b748f1fa6ec9ca5dc20bba701e542a
Feras Dahrooge <87308570+ferasdahrooge@users.noreply.github.com>
2023-03-04 13:23:25 -0700

    Clean up some css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home';
-import {Notification} from "./pages/Notification";
-import {Update} from "./pages/update";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Notification } from "./pages/Notification/Notification";
 
 function App() {
   return (
-      <>
-        <Router>
-          <Routes>
-            <Route exact path='/' element={<Home/>}/>
-            <Route exact path='/notifications' element={<Notification/>}/>
-            <Route path='/update' element={<Update />}></Route>
-          </Routes>
-        </Router>
-      </>
-  )}
+    <>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/notifications' element={<Notification />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
 
 export default App;
